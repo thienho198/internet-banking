1ca7a991057add49804c4be1a598aec94f456413
Datnt <thanhdatus16@gmail.com>
2020-06-18 14:44:37 +0700

    Test

--- controllers/bankController.js
+++ controllers/bankController.js
@@ -37,7 +37,7 @@ exports.rgpAddMoneyByStk = async (req, res, next) => {
     paymentAccount.balance = paymentAccount.balance + amountOfMoney;
     console.log(paymentAccount.balance);
     await paymentAccount.save();
-    const customer = await Customer.findOne(paymentAccount._id);
+    const customer = await Customer.findOne((stk: paymentAccount._id));
     let obj = { success: true, username: customer.name };
     responsePgp(res, obj, 202);
   } catch (err) {
