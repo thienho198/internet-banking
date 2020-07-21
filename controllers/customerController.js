3d2a8e49523770de7b0cffd95b10c12c4034f6a9
Datnt <thanhdatus16@gmail.com>
2020-07-21 22:55:11 +0700

    thay doi 1 so code

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -62,56 +62,48 @@ exports.getListDeptReminderRemind = (req, res, next) => {
 exports.deleteDeptReminder = async (req, res, next) => {
   const { id, message } = req.body;
   const deptReminder = await DeptReminder.findById(id);
-  const accessToken = req.headers['x-access-token'];
   if (!id) {
     res.status(400).json({ err: 'Dept reminder not exist' });
   }
   try {
-    jwt.verify(
-      accessToken,
-      process.env.JWT_SECRET,
-      { ignoreExpiration: true },
-      async function (err, payload) {
-        const idDeptRemind = payload.id;
-        const stkremind = await PaymentAccount.findOne({
-          stk: deptReminder.stkRemind,
-        });
-        const stkwasremined = await PaymentAccount.findOne({
-          stk: deptReminder.stkWasRemined,
-        });
-        const customerRemind = await Customer.findOne({
-          paymentAccountId: stkremind,
-        });
-        const customerWasRemined = await Customer.findOne({
-          paymentAccountId: stkwasremined,
-        });
-        const listRemindUpdate = customerRemind.listDeptReminders.filter(
-          (item) => item.deptReminderId.toString() !== id.toString()
-        );
-        const listWasRemindUpdate = customerWasRemined.listDeptReminders.filter(
-          (item) => item.deptReminderId.toString() !== id.toString()
-        );
-        const customerRemindNotify = customerRemind.notifications;
-        customerRemind.listDeptReminders = listRemindUpdate;
-        customerWasRemined.listDeptReminders = listWasRemindUpdate;
-        // Them notify
-        if (idDeptRemind === customerRemind.id)
-          customerWasRemined.notifications.push({
-            notify:
-              `So tai khoan ${deptReminder.stkRemind} da xoa nhac no voi noi dung:  ` +
-              message,
-          });
-        else
-          customerRemind.notifications.push({
-            notify:
-              `So tai khoan ${deptReminder.stkWasRemined} da xoa nhac no voi noi dung:  ` +
-              message,
-          });
-        await customerWasRemined.save();
-        await customerRemind.save();
-        await deptReminder.remove();
-      }
+    const idDeptRemind = req.customer.id;
+    const stkremind = await PaymentAccount.findOne({
+      stk: deptReminder.stkRemind,
+    });
+    const stkwasremined = await PaymentAccount.findOne({
+      stk: deptReminder.stkWasRemined,
+    });
+    const customerRemind = await Customer.findOne({
+      paymentAccountId: stkremind,
+    });
+    const customerWasRemined = await Customer.findOne({
+      paymentAccountId: stkwasremined,
+    });
+    const listRemindUpdate = customerRemind.listDeptReminders.filter(
+      (item) => item.deptReminderId.toString() !== id.toString()
     );
+    const listWasRemindUpdate = customerWasRemined.listDeptReminders.filter(
+      (item) => item.deptReminderId.toString() !== id.toString()
+    );
+    const customerRemindNotify = customerRemind.notifications;
+    customerRemind.listDeptReminders = listRemindUpdate;
+    customerWasRemined.listDeptReminders = listWasRemindUpdate;
+    // Them notify
+    if (idDeptRemind === customerRemind.id)
+      customerWasRemined.notifications.push({
+        notify:
+          `So tai khoan ${deptReminder.stkRemind} da xoa nhac no voi noi dung:  ` +
+          message,
+      });
+    else
+      customerRemind.notifications.push({
+        notify:
+          `So tai khoan ${deptReminder.stkWasRemined} da xoa nhac no voi noi dung:  ` +
+          message,
+      });
+    await customerWasRemined.save();
+    await customerRemind.save();
+    await deptReminder.remove();
     res.status(202).json({ succes: true });
   } catch (err) {
     console.log(err);
@@ -120,43 +112,34 @@ exports.deleteDeptReminder = async (req, res, next) => {
 };
 
 exports.createListRemind = async (req, res, next) => {
-  const accessToken = req.headers['x-access-token'];
   let { stk, nameRemind, bank } = req.body;
   try {
-    jwt.verify(
-      accessToken,
-      process.env.JWT_SECRET,
-      { ignoreExpiration: true },
-      async function (err, payload) {
-        const { id } = payload;
-        const customer = await Customer.findById(id);
-        const findStk = customer.listAccountRemind.find(
-          (item) => item.stk === stk.toString()
-        );
-        if (!bank || bank === 'G16BANK') {
-          const accountRemind = await PaymentAccount.findOne({ stk });
-          if (!accountRemind)
-            return res.status(400).json({
-              success: false,
-              err: 'This account does not exist in bank',
-            });
-          if (!nameRemind) {
-            nameRemind = accountRemind.name;
-          }
-        }
-        if (!findStk) {
-          customer.listAccountRemind.push({ stk, nameRemind, bank });
-          await customer.save();
-          return res
-            .status(200)
-            .json({ success: true, data: customer.listAccountRemind });
-        }
+    const customer = req.customer;
+    const findStk = customer.listAccountRemind.find(
+      (item) => item.stk === stk.toString()
+    );
+    if (!bank || bank === 'G16BANK') {
+      const accountRemind = await PaymentAccount.findOne({ stk });
+      if (!accountRemind)
         return res.status(400).json({
           success: false,
-          err: 'This account is already in the remind list',
+          err: 'This account does not exist in bank',
         });
+      if (!nameRemind) {
+        nameRemind = accountRemind.name;
       }
-    );
+    }
+    if (!findStk) {
+      customer.listAccountRemind.push({ stk, nameRemind, bank });
+      await customer.save();
+      return res
+        .status(200)
+        .json({ success: true, data: customer.listAccountRemind });
+    }
+    return res.status(400).json({
+      success: false,
+      err: 'This account is already in the remind list',
+    });
   } catch (err) {
     res.status(400).json({ success: false, err });
   }
@@ -219,67 +202,49 @@ exports.updateListRemind = async (req, res, next) => {
 };
 
 exports.deleteListRemind = async (req, res, next) => {
-  const accessToken = req.headers['x-access-token'];
   let { stk } = req.body;
   try {
-    jwt.verify(
-      accessToken,
-      process.env.JWT_SECRET,
-      { ignoreExpiration: true },
-      async function (err, payload) {
-        const { id } = payload;
-        const customer = await Customer.findById(id);
-        let check = false;
-        let updateListRemind = customer.listAccountRemind.filter((item) => {
-          if (item.stk === stk.toString()) check = true;
-          return item.stk !== stk.toString();
-        });
-        if (check) {
-          customer.listAccountRemind = updateListRemind;
-          await customer.save();
-          return res
-            .status(200)
-            .json({ success: true, data: customer.listAccountRemind });
-        }
-        return res.status(400).json({
-          success: false,
-          err: 'Account not found in list',
-        });
-      }
-    );
+    const customer = req.customer;
+    let check = false;
+    let updateListRemind = customer.listAccountRemind.filter((item) => {
+      if (item.stk === stk.toString()) check = true;
+      return item.stk !== stk.toString();
+    });
+    if (check) {
+      customer.listAccountRemind = updateListRemind;
+      await customer.save();
+      return res
+        .status(200)
+        .json({ success: true, data: customer.listAccountRemind });
+    }
+    return res.status(400).json({
+      success: false,
+      err: 'Account not found in list',
+    });
   } catch (err) {
     res.status(400).json({ success: false, err });
   }
 };
 
 exports.historyDebtRemind = async (req, res, next) => {
-  const accessToken = req.headers['x-access-token'];
   try {
-    jwt.verify(
-      accessToken,
-      process.env.JWT_SECRET,
-      { ignoreExpiration: true },
-      async function (err, payload) {
-        const { id } = payload;
-        const customer = await Customer.findById(id);
-        const paymentAccount = await PaymentAccount.findById(
-          customer.paymentAccountId
-        );
-        const history = await History.find();
-        const historyDebtRemind = history.filter(
-          (item) =>
-            ((item.accountReceive === paymentAccount.stk &&
-              item.bankReceiver === 'G16BANK') ||
-              (item.accountSender === paymentAccount.stk &&
-                item.bankSender === 'G16BANK')) &&
-            item.category === 'DeptPay'
-        );
-        res.status(200).json({
-          success: true,
-          historyDebtRemind,
-        });
-      }
+    const customer = await req.customer;
+    const paymentAccount = await PaymentAccount.findById(
+      customer.paymentAccountId
     );
+    const history = await History.find();
+    const historyDebtRemind = history.filter(
+      (item) =>
+        ((item.accountReceive === paymentAccount.stk &&
+          item.bankReceiver === 'G16BANK') ||
+          (item.accountSender === paymentAccount.stk &&
+            item.bankSender === 'G16BANK')) &&
+        item.category === 'DeptPay'
+    );
+    res.status(200).json({
+      success: true,
+      historyDebtRemind,
+    });
   } catch (err) {
     console.log(err);
     res.status(400).json({ err });
@@ -287,30 +252,21 @@ exports.historyDebtRemind = async (req, res, next) => {
 };
 
 exports.historyReceive = async (req, res, next) => {
-  const accessToken = req.headers['x-access-token'];
   try {
-    jwt.verify(
-      accessToken,
-      process.env.JWT_SECRET,
-      { ignoreExpiration: true },
-      async function (err, payload) {
-        const { id } = payload;
-        const customer = await Customer.findById(id);
-        const paymentAccount = await PaymentAccount.findById(
-          customer.paymentAccountId
-        );
-        const history = await History.find();
-        const historyReceive = history.filter(
-          (item) =>
-            item.accountReceive === paymentAccount.stk &&
-            item.bankReceiver === 'G16BANK'
-        );
-        res.status(200).json({
-          success: true,
-          historyReceive,
-        });
-      }
+    const customer = req.customer;
+    const paymentAccount = await PaymentAccount.findById(
+      customer.paymentAccountId
     );
+    const history = await History.find();
+    const historyReceive = history.filter(
+      (item) =>
+        item.accountReceive === paymentAccount.stk &&
+        item.bankReceiver === 'G16BANK'
+    );
+    res.status(200).json({
+      success: true,
+      historyReceive,
+    });
   } catch (err) {
     console.log(err);
     res.status(400).json({ err });
@@ -318,30 +274,21 @@ exports.historyReceive = async (req, res, next) => {
 };
 
 exports.historyTransfer = async (req, res, next) => {
-  const accessToken = req.headers['x-access-token'];
   try {
-    jwt.verify(
-      accessToken,
-      process.env.JWT_SECRET,
-      { ignoreExpiration: true },
-      async function (err, payload) {
-        const { id } = payload;
-        const customer = await Customer.findById(id);
-        const paymentAccount = await PaymentAccount.findById(
-          customer.paymentAccountId
-        );
-        const history = await History.find();
-        const historyTransfer = history.filter(
-          (item) =>
-            item.accountSender === paymentAccount.stk &&
-            item.bankSender === 'G16BANK'
-        );
-        res.status(200).json({
-          success: true,
-          historyTransfer,
-        });
-      }
+    const customer = req.customer;
+    const paymentAccount = await PaymentAccount.findById(
+      customer.paymentAccountId
     );
+    const history = await History.find();
+    const historyTransfer = history.filter(
+      (item) =>
+        item.accountSender === paymentAccount.stk &&
+        item.bankSender === 'G16BANK'
+    );
+    res.status(200).json({
+      success: true,
+      historyTransfer,
+    });
   } catch (err) {
     console.log(err);
     res.status(400).json({ err });
@@ -406,53 +353,42 @@ exports.sendOTP = async (req, res, next) => {
 };
 
 exports.transferMoney = async (req, res, next) => {
-  const accessToken = req.headers['x-access-token'];
   const { stk, amountOfMoney, message, otpcode, category } = req.body;
   try {
-    jwt.verify(
-      accessToken,
-      process.env.JWT_SECRET,
-      { ignoreExpiration: true },
-      async function (err, payload) {
-        const { id } = payload;
-        const customer = await Customer.findById(id);
-        if (!customer) {
-          return res
-            .status(400)
-            .json({ success: false, err: 'User not exists' });
-        }
-        if (customer.OTP !== otpcode) {
-          return res.status(403).json({ success: false, err: 'Invalid OTP' });
-        }
-        const userAccount = await PaymentAccount.findById(
-          customer.paymentAccountId
-        );
-        const allFee = amountOfMoney + parseInt(process.env.FEETRANSFER);
-        if (userAccount.balance < allFee)
-          return res
-            .status(406)
-            .json({ success: false, err: 'You dont have enough money' });
-
-        const transferAccount = await PaymentAccount.findOne({ stk: stk });
-        transferAccount.balance = transferAccount.balance + amountOfMoney;
-        userAccount.balance = userAccount.balance - allFee;
-        customer.OTP = undefined;
-        await customer.save();
-        await userAccount.save();
-        await transferAccount.save();
-        await History.create({
-          operator: 'Customer',
-          accountSender: userAccount.stk,
-          sender: userAccount.name,
-          accountReceive: stk,
-          receiver: transferAccount.name,
-          message,
-          amountOfMoney,
-          category,
-        });
-        res.json({ success: true, userAccount, transferAccount, message });
-      }
+    const customer = req.customer;
+    if (!customer) {
+      return res.status(400).json({ success: false, err: 'User not exists' });
+    }
+    if (customer.OTP !== otpcode) {
+      return res.status(403).json({ success: false, err: 'Invalid OTP' });
+    }
+    const userAccount = await PaymentAccount.findById(
+      customer.paymentAccountId
     );
+    const allFee = amountOfMoney + parseInt(process.env.FEETRANSFER);
+    if (userAccount.balance < allFee)
+      return res
+        .status(406)
+        .json({ success: false, err: 'You dont have enough money' });
+
+    const transferAccount = await PaymentAccount.findOne({ stk: stk });
+    transferAccount.balance = transferAccount.balance + amountOfMoney;
+    userAccount.balance = userAccount.balance - allFee;
+    customer.OTP = undefined;
+    await customer.save();
+    await userAccount.save();
+    await transferAccount.save();
+    await History.create({
+      operator: 'Customer',
+      accountSender: userAccount.stk,
+      sender: userAccount.name,
+      accountReceive: stk,
+      receiver: transferAccount.name,
+      message,
+      amountOfMoney,
+      category,
+    });
+    res.json({ success: true, userAccount, transferAccount, message });
   } catch (err) {
     console.log(err);
     res.json({ success: false, err });
